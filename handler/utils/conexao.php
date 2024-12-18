fc8fae9a5c4c64268a99073e0443ba70d77cf98d
oCrazyoff <walyssonribeiro3@gmail.com>
2024-12-18 20:40:01 -0300

    add estrutura de pastas simples

---
+++ handler/utils/conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+/*$servidor = "sql112.infinityfree.com";
+$usuario = "if0_37944711";
+$senha = "obLVaJpCA6GF";
+$dbname = "if0_37944711_psf";*/
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "psf";
+
+$conn = new mysqli($servidor, $usuario, $senha, $dbname);
+
+if ($conn->connect_error) {
+    die("ERRO! " . $conn->connect_error);
+}
