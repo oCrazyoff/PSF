c37dbdba4f122dca83eb3057fe11a3377d26463a
Leandr0Arantes <leandro.arantes@edu.uniube.br>
2024-12-19 20:35:33 -0300

    teste de login

--- logar.php
+++ logar.php
@@ -1,15 +1,18 @@
-<?php 
+<?php
 include("handler/utils/conexao.php");
 
 $email = $_POST["email"];
 $senha = $_POST["senha"];
 
-echo($email.$senha);
+echo ($email . $senha);
 
+$sql("SELECT * FROM pessoas WHERE email = ?, senha = ?");
+$stmt = $conn->prepare($sql);
 
+$stmt->bind_param("ss", $email, $senha);
+$stmt->execute();
+$stmt->bind_result($nome);
+$stmt->fetch();
 
-
-
-
-
+echo($nome);
 ?>
