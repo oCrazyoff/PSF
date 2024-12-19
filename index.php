d01222e4e9d2d1b3a2b70be312636ef9bef45742
Leandr0Arantes <leandro.arantes@edu.uniube.br>
2024-12-19 20:22:23 -0300

    teste

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Logar</title>
 </head>
 
 <body>
-    <h1>Teste</h1>
+    <form action="logar.php" method="post">
+        <label for="usuario">email</label>
+        <input type="email" name="email" id="email">
+        <label for="senha">Senha</label>
+        <button type="submit">Entrar</button>
+    </form>
 </body>
 
 </html>
---
+++ logar.php
@@ -0,0 +1,15 @@
+<?php 
+include("handler/utils/conexao.php");
+
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+
+echo($email.$senha);
+
+
+
+
+
+
+
+?>
