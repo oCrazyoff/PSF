ce8b9ba2e63c0e57b0966a12b307b50a3c2bc4e6
Leandr0Arantes <leandro.arantes@edu.uniube.br>
2024-12-24 23:09:19 -0300

    Btn sair

--- includes/menu.php
+++ includes/menu.php
@@ -3,6 +3,6 @@
         <li><a href="#"><i class="fas fa-home"></i> Home</a></li>
         <li><a href="#"><i class="fas fa-user"></i> Perfil</a></li>
         <li><a href="#"><i class="fas fa-cogs"></i> Configurações</a></li>
-        <li><a href="#"><i class="fas fa-sign-out-alt"></i> Sair</a></li>
+        <li><a href="../handler/usuario/sair.php"><i class="fas fa-sign-out-alt"></i> Sair</a></li>
     </ul>
 </div>
