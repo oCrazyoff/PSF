4423369cf22ab558143e5eb559a3a4203d1db882
oCrazyoff <walyssonribeiro3@gmail.com>
2024-12-30 00:39:58 -0300

    add validação de usuarios

--- auth/logar.php
+++ auth/logar.php
@@ -4,19 +4,20 @@ include("../database/utils/conexao.php");
 $email = $_POST["email"];
 $senha = $_POST["senha"];
 
-$sql = "SELECT nome FROM pessoas WHERE email = ? AND senha = ?";
+$sql = "SELECT nome, cargo FROM pessoas WHERE email = ? AND senha = ?";
 
 $stmt = $conn->prepare($sql);
 
 if ($stmt) {
     $stmt->bind_param("ss", $email, $senha);
     $stmt->execute();
-    $stmt->bind_result($nome);
+    $stmt->bind_result($nome, $cargo);
     $stmt->fetch();
 
     if ($nome != '') {
         session_start();
         $_SESSION["nome"] = $nome;
+        $_SESSION['cargo'] = $cargo;
         header("Location: ../pages/inicio.php");
     } else {
         $resposta = "Usuário ou senha incorreto!";
