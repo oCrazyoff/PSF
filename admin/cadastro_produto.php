6aff22811d9a7bc50005a65ccc8485c2832b1789
LuisGuilherme2 <luisartes442@gmail.com>
2024-12-30 21:27:37 -0300

    add formulario cadastro produto

--- admin/cadastro_produto.php
+++ admin/cadastro_produto.php
@@ -19,7 +19,7 @@ include("../auth/valida.php");
         <form action="auth/logar.php" method="post">
             <h2 class="form-title">Cadastrar Produto</h2>
             <div class="form-group">
-                <label for="email">Nome</label>
+                <label for="nome">Nome</label>
                 <div class="input-group">
                     <span class="input-group-text"><i class="fa-solid fa-pen"></i></span>
                     <input type="text" class="form-control" name="nome" id="nome" placeholder="Digite nome do produto"
@@ -35,7 +35,7 @@ include("../auth/valida.php");
                 </div>
             </div>
             <div class="form-group">
-                <label for="email">Fornecedor</label>
+                <label for="fornecedor">Fornecedor</label>
                 <div class="input-group">
                     <span class="input-group-text"><i class="fa-solid fa-truck"></i></span>
                     <input type="text" class="form-control" name="fornecedor" id="fornecedor" placeholder="Digite nome do Fornecedor"
@@ -43,10 +43,51 @@ include("../auth/valida.php");
                 </div>
             </div>
             <div class="form-group">
-                <label for="senha">Código de Barras</label>
+                <label for="marca">Marca</label>
                 <div class="input-group">
-                    <span class="input-group-text"><i class="fa-solid fa-barcode"></i></span>
-                    <input type="text" class="form-control" name="codigo" id="codigo" placeholder="Digite o Código de Barras"
+                    <span class="input-group-text"><i class="fa-solid fa-tags"></i></span>
+                    <input type="text" class="form-control" name="marca" id="marca" placeholder="Digite o nome da Marca"
+                        required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="grupo">Grupo</label>
+                <div class="input-group">
+                    <span class="input-group-text"><i class="fa-solid fa-boxes-stacked"></i></span>
+                    <input type="text" class="form-control" name="grupo" id="grupo" placeholder="Digite nome do Grupo"
+                        required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="preco_custo">Preço de Custo</label>
+                <div class="input-group">
+                    <span class="input-group-text"><i class="fa-solid fa-money-bill"></i></span>
+                    <input type="number" class="form-control" name="preco_custo" id="preco_custo" placeholder="Digite o Preço de Custo"
+                        required>
+                </div>
+                 
+            </div>
+            <div class="form-group">
+                <label for="preco_venda">Preço de Venda</label>
+                <div class="input-group">
+                    <span class="input-group-text"><i class="fa-solid fa-hand-holding-dollar"></i></span>   
+                    <input type="number" class="form-control" name="preco_venda" id="preco_venda" placeholder="Digite o Preço de Venda"
+                        required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="quantidade">Quantidade</label>
+                <div class="input-group">
+                    <span class="input-group-text"><i class="fa-solid fa-list-ol"></i></span>
+                    <input type="number" class="form-control" name="quantidade" id="quantidade" placeholder="Digite a Quantidade"
+                        required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="validade">Validade</label>
+                <div class="input-group">
+                    <span class="input-group-text"><i class="fa-solid fa-calendar-check"></i></span>
+                    <input type="date" class="form-control" name="validade" id="validade" placeholder="Digite a Validade"
                         required>
                 </div>
             </div>
