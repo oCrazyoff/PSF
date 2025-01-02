f063db9ae150a23044bc47d2e3c194f30b281d5e
oCrazyoff <walyssonribeiro3@gmail.com>
2025-01-01 22:58:12 -0300

    add favicon

--- includes/link_head.php
+++ includes/link_head.php
@@ -1,3 +1,4 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+<link rel="shortcut icon" href="../assets/img/icone.png" type="image/x-icon">
 <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/header.css?v=<?php echo time(); ?>">
 <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/menu.css?v=<?php echo time(); ?>">
