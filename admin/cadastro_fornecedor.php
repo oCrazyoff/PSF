54056f5baa9e174c0e48f1061ffe6d2b0be67a55
oCrazyoff <walyssonribeiro3@gmail.com>
2025-01-03 21:46:33 -0300

    add form container

--- admin/cadastro_fornecedor.php
+++ admin/cadastro_fornecedor.php
@@ -19,53 +19,61 @@ include("../auth/config.php");
     <?php include("../includes/header.php") ?>
     <?php include("../includes/menu.php") ?>
     <div class="content">
-        <div class="form-container" id="index">
-            <form action="../database/cadastrar/cadastrar_fornecedor.php" method="post">
-                <h2 class="form-title">Cadastrar Fornecedor</h2>
-                <div class="form-group">
-                    <label for="nome">Nome</label>
-                    <div class="input-group">
-                        <span class="input-group-text"><i class="fas fa-envelope"></i></span>
-                        <input type="text" class="form-control" name="nome" id="nome" placeholder="Digite seu Nome" required>
+        <div class="form-container">
+            <div class="form-container" id="index">
+                <form action="../database/cadastrar/cadastrar_fornecedor.php" method="post">
+                    <h2 class="form-title">Cadastrar Fornecedor</h2>
+                    <div class="form-group">
+                        <label for="nome">Nome</label>
+                        <div class="input-group">
+                            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                            <input type="text" class="form-control" name="nome" id="nome" placeholder="Digite seu Nome"
+                                required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="cnpj">CNPJ</label>
-                    <div class="input-group">
-                        <span class="input-group-text"><i class="fas fa-lock"></i></span>
-                        <input type="text" class="form-control" name="cnpj" id="cnpj" placeholder="Digite sua CNPJ" required>
+                    <div class="form-group">
+                        <label for="cnpj">CNPJ</label>
+                        <div class="input-group">
+                            <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                            <input type="text" class="form-control" name="cnpj" id="cnpj" placeholder="Digite sua CNPJ"
+                                required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="razao_social">Razão Social</label>
-                    <div class="input-group">
-                        <span class="input-group-text"><i class="fas fa-lock"></i></span>
-                        <input type="text" class="form-control" name="razao_social" id="razao_social" placeholder="Digite sua Razão Social" required>
+                    <div class="form-group">
+                        <label for="razao_social">Razão Social</label>
+                        <div class="input-group">
+                            <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                            <input type="text" class="form-control" name="razao_social" id="razao_social"
+                                placeholder="Digite sua Razão Social" required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="nome_fantasia">Nome Fantasia</label>
-                    <div class="input-group">
-                        <span class="input-group-text"><i class="fas fa-lock"></i></span>
-                        <input type="text" class="form-control" name="nome_fantasia" id="nome_fantasia" placeholder="Digite seu Nome Fantásia" required>
+                    <div class="form-group">
+                        <label for="nome_fantasia">Nome Fantasia</label>
+                        <div class="input-group">
+                            <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                            <input type="text" class="form-control" name="nome_fantasia" id="nome_fantasia"
+                                placeholder="Digite seu Nome Fantásia" required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="endereco">Endereço</label>
-                    <div class="input-group">
-                        <span class="input-group-text"><i class="fas fa-lock"></i></span>
-                        <input type="text" class="form-control" name="endereco" id="endereco" placeholder="Digite seu Endereço" required>
+                    <div class="form-group">
+                        <label for="endereco">Endereço</label>
+                        <div class="input-group">
+                            <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                            <input type="text" class="form-control" name="endereco" id="endereco"
+                                placeholder="Digite seu Endereço" required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="contato">Contato</label>
-                    <div class="input-group">
-                        <span class="input-group-text"><i class="fas fa-lock"></i></span>
-                        <input type="text" class="form-control" name="contato" id="contato" placeholder="Digite seu Contato" required>
+                    <div class="form-group">
+                        <label for="contato">Contato</label>
+                        <div class="input-group">
+                            <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                            <input type="text" class="form-control" name="contato" id="contato"
+                                placeholder="Digite seu Contato" required>
+                        </div>
                     </div>
-                </div>
-                <button type="submit" class="btn btn-primary btn-block mt-3">Entrar</button>
-            </form>
+                    <button type="submit" class="btn btn-primary btn-block mt-3">Entrar</button>
+                </form>
+            </div>
         </div>
     </div>
 </body>
