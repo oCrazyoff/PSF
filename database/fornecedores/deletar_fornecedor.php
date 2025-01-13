ecf6f02325568c9bdaa301623f07c83f89e4d246
Leandr0Arantes <leandro.arantes@edu.uniube.br>
2025-01-13 19:35:26 -0300

    Mexi em uns negocio que esqueci no deletar de fornecedores e funcionarios

--- database/fornecedores/deletar_fornecedor.php
+++ database/fornecedores/deletar_fornecedor.php
@@ -6,23 +6,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $cnpj = $_POST["cnpj"];
     $status = $_POST["status"];
     if ((isset($_POST["cnpj"])) and (!empty($_POST))) {
-        if($status == 1){
+        if ($status == 1) {
             $sql = "UPDATE fornecedores SET status = 0 WHERE cnpj = ?";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("s", $cnpj);
-        } else {
-            $sql = "UPDATE fornecedores SET status = 1 WHERE cnpj = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("s", $cnpj);
-        }
 
-        if ($status == 1) {
             if ($stmt->execute()) {
                 $_SESSION['resposta'] = "Fornecedor deletado com sucesso!";
             } else {
                 $_SESSION['resposta'] = "Erro ao deletar Fornecedor: " . $stmt->error;
             }
         } else {
+            $sql = "UPDATE fornecedores SET status = 1 WHERE cnpj = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $cnpj);
+
             if ($stmt->execute()) {
                 $_SESSION['resposta'] = "Fornecedor reintegrado com sucesso!";
             } else {
--- database/funcionarios/deletar_funcionario.php
+++ database/funcionarios/deletar_funcionario.php
@@ -10,26 +10,23 @@ if ($status == 1) {
     $sql = "UPDATE funcionarios SET status = 0, data_demissao = ? WHERE id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ss", $hoje, $id);
-} else {
-    $sql = "UPDATE funcionarios SET status = 1, data_demissao = null WHERE id = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $id);
-}
-
-if ($status == 1) {
+    
     if ($stmt->execute()) {
         $_SESSION['resposta'] = "Funcionário deletado com sucesso!";
     } else {
         $_SESSION['resposta'] = "Erro ao deletar Funcionário: " . $stmt->error;
     }
 } else {
+    $sql = "UPDATE funcionarios SET status = 1, data_demissao = null WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $id);
+
     if ($stmt->execute()) {
         $_SESSION['resposta'] = "Funcionário reintegrado com sucesso!";
     } else {
         $_SESSION['resposta'] = "Erro ao reintegrar Funcionário: " . $stmt->error;
     }
 }
-
 
 header("Location: ../../admin/funcionarios/funcionarios.php");
 exit;
