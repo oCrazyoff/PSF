7bce1258010a07f256b5b1c3c07b8fbd8752b317
Leandr0Arantes <leandro.arantes@edu.uniube.br>
2025-01-17 18:28:16 -0300

    Mudança nas validações de logar e cadastrar cliente pelo form de login, agora está mais seguro

--- auth/logar.php
+++ auth/logar.php
@@ -1,27 +1,45 @@
 <?php
 include("../database/utils/conexao.php");
 
-$email = $_POST["email"];
-$senha = $_POST["senha"];
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $senha = htmlspecialchars($_POST["senha"]);
 
-$sql = "SELECT nome, cargo, senha FROM pessoas WHERE email = ?";
-
-$stmt = $conn->prepare($sql);
-
-if ($stmt) {
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $stmt->bind_result($nome, $cargo, $senha_bd);
-    $stmt->fetch();
-
-    if ($nome != '' && password_verify($senha, $senha_bd)) {
-        session_start();
-        $_SESSION["nome"] = $nome;
-        $_SESSION['cargo'] = $cargo;
-        header("Location: ../pages/inicio.php");
-    } else {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $resposta = "Usuário ou senha incorreto!";
         header("Location: ../index.php?resposta=$resposta");
-        die;
+        exit;
     }
+
+    if (isset($_POST["email"]) and isset($_POST["senha"])) {
+        $sql = "SELECT nome, cargo, senha FROM pessoas WHERE email = ?";
+
+        $stmt = $conn->prepare($sql);
+
+        if ($stmt) {
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($nome, $cargo, $senha_bd);
+            $stmt->fetch();
+
+            if ($nome != '' && password_verify($senha, $senha_bd)) {
+                session_start();
+                $_SESSION["nome"] = $nome;
+                $_SESSION['cargo'] = $cargo;
+                header("Location: ../pages/inicio.php");
+                exit;
+            } else {
+                $resposta = "Usuário ou senha incorreto!";
+                header("Location: ../index.php?resposta=$resposta");
+                exit;
+            }
+        }
+    } else {
+        $_SESSION['resposta'] = "Variável POST ínvalida!";
+    }
+} else {
+    $_SESSION['resposta'] = "Método de solicitação ínvalido!";
 }
+
+header("Location: ../index.php?resposta=$resposta");
+exit;
--- database/clientes/cadastrar_cliente.php
+++ database/clientes/cadastrar_cliente.php
@@ -1,28 +1,52 @@
 <?php
 include("../utils/conexao.php");
 
-$nome = $_POST["nome"];
-$email = $_POST["email"];
-$senha = $_POST["senha"];
-$senha_hash = password_hash($senha, PASSWORD_BCRYPT);
-$confirma_senha = $_POST["confirma_senha"];
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+    $nome = htmlspecialchars($_POST["nome"]);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $senha = htmlspecialchars($_POST["senha"]);
+    $confirma_senha = htmlspecialchars($_POST["confirma_senha"]);
 
-if ($senha != $confirma_senha) {
-    $resposta = "Usuário ou senha incorreto!";
-    header("Location: ../../pages/cadastrese.php?resposta=$resposta");
-    die();
-}
+    if (isset($_POST["nome"]) and isset($_POST["email"]) and isset($_POST["senha"]) and isset($_POST["confirma_senha"])) {
+        if ($senha == $confirma_senha) {
+            $senha_hash = password_hash($senha, PASSWORD_BCRYPT);
+        } else {
+            $resposta = "As senhas não estão iguais!";
+            header("Location: ../../pages/cadastrese.php?resposta=$resposta");
+            exit;
+        }
 
-$sql = "INSERT INTO pessoas (nome, email, senha) VALUES (?, ?, ?)";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $nome, $email, $senha_hash);
+        try {
+            $sql = "INSERT INTO pessoas (nome, email, senha) VALUES (?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sss", $nome, $email, $senha_hash);
 
-if ($stmt->execute()) {
-    $resposta = "Usuário cadastrado com sucesso!";
-    header("Location: ../../index.php?resposta=$resposta");
-    die();
+            if ($stmt->execute()) {
+                $resposta = "Usuário cadastrado com sucesso!";
+                header("Location: ../../index.php?resposta=$resposta");
+                exit;
+            } else {
+                $resposta = "Usuário deu erro!";
+                header("Location: ../../index.php?resposta=$resposta");
+                exit;
+            }
+        } catch (Exception $erro_email) {
+            if ($erro_email->getCode() == 1062) {
+                $resposta = "Email já cadastrado!";
+                header("Location: ../../index.php?resposta=$resposta");
+                exit;
+            } else {
+                $resposta = "Erro ao cadastrar usuário!";
+                header("Location: ../../index.php?resposta=$resposta");
+                exit;
+            }
+        }
+    } else {
+        $_SESSION['resposta'] = "Variável POST ínvalida!";
+    }
 } else {
-    $resposta = "Usuário deu erro!";
-    header("Location: ../../index.php?resposta=$resposta");
-    die();
+    $_SESSION['resposta'] = "Método de solicitação ínvalido!";
 }
+
+header("Location: ../index.php?resposta=$resposta");
+exit;
