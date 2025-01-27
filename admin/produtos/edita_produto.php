f1d6af55632ac7b9d61a3eba23bd0fd4a010b432
oCrazyoff <walyssonribeiro3@gmail.com>
2025-01-27 20:22:09 -0300

    add phpinfo page, update image handling in product edit, and improve database connection logic

--- admin/produtos/edita_produto.php
+++ admin/produtos/edita_produto.php
@@ -4,22 +4,16 @@ include("../../auth/valida.php");
 include("../../database/utils/conexao.php");
 
 $id = $_POST['id'];
-$sqlProduto = "SELECT * FROM produtos WHERE id = '$id'";
-$resultadoProduto = $conn->query($sqlProduto);
 
-while ($rowProduto = $resultadoProduto->fetch_assoc()) {
-    $nome = $rowProduto['nome'];
-    $codigo_barra = $rowProduto['codigo_barra'];
-    $fornecedor = $rowProduto['fornecedor'];
-    $marca = $rowProduto['marca'];
-    $grupo = $rowProduto['grupo'];
-    $subgrupo = $rowProduto['subgrupo'];
-    $preco_custo = $rowProduto['preco_custo'];
-    $preco_venda = $rowProduto['preco_venda'];
-    $quantidade = $rowProduto['quantidade'];
-    $validade = $rowProduto['validade'];
-    $imagem = $rowProduto["imagem"];
-}
+$sql = "SELECT * FROM produtos WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$result = $stmt->get_result();
+$produto = $result->fetch_assoc();
+$stmt->close();
+
+$imgSrc = 'data:image/jpeg;base64,' . base64_encode($produto['imagem']);
 
 ?>
 <!DOCTYPE html>
@@ -28,7 +22,7 @@ while ($rowProduto = $resultadoProduto->fetch_assoc()) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar <?php echo $nome ?></title>
+    <title>Editar <?= $produto['nome'] ?> </title>
     <?php include("../../includes/link_head.php") ?>
     <link rel="stylesheet" href="../../assets/css/form.css?v=<?php echo time(); ?>">
 </head>
@@ -38,13 +32,34 @@ while ($rowProduto = $resultadoProduto->fetch_assoc()) {
     <?php include("../../includes/menu.php") ?>
     <div class="content">
         <div class="form-container" id="large-form">
-            <h2 class="form-title">Editar <?php echo $nome ?></h2>
-            <form action="../../database/produtos/editar_produto.php" method="post">
+            <h2 class="form-title">Imagem do Produto</h2>
+            <form action="../../database/produtos/editar_produto.php" method="post" enctype="multipart/form-data">
+                <div class="input-file">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <img id="preview"
+                                src=" <?= (($produto['imagem'] != null || $produto['imagem'] != "") ? $imgSrc : "../../assets/img/placeholder.png") ?> "
+                                alt="Pré-visualização da Imagem">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="imagem">Arquivo da imagem</label>
+                        <div class="input-group">
+                            <span class="input-group-text"><i class="fa-solid fa-image"></i></span>
+                            <input type="file" class="form-control" name="imagem" id="imagem"
+                                accept="image/png, image/jpeg" required>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <div class="form-container" id="large-form">
+            <h2 class="form-title">Informações Gerais</h2>
+            <div class="card-form">
                 <div class="form-group">
                     <label for="nome">Nome</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-pen"></i></span>
-                        <input type="text" class="form-control" value="<?php echo $nome ?>" name="nome" id="nome"
+                        <input type="text" class="form-control" value="<?= $produto['nome'] ?>" name="nome" id="nome"
                             placeholder="Digite nome do produto" required>
                     </div>
                 </div>
@@ -52,7 +67,7 @@ while ($rowProduto = $resultadoProduto->fetch_assoc()) {
                     <label for="codigo">Código de Barras</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-barcode"></i></span>
-                        <input type="text" class="form-control" value="<?php echo $codigo_barra ?>" name="codigo"
+                        <input type="text" class="form-control" value="<?= $produto['codigo_barra'] ?>" name="codigo"
                             id="codigo" placeholder="Digite o Código de Barras">
                     </div>
                 </div>
@@ -60,16 +75,16 @@ while ($rowProduto = $resultadoProduto->fetch_assoc()) {
                     <label for="fornecedor">Fornecedores</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-truck"></i></span>
-                        <select name="fornecedor" id="fornecedor">
+                        <select name="fornecedor" id="fornecedor" required>
                             <?php
-                            $sqlFornecedores = "SELECT * FROM fornecedores WHERE status = 1";
-                            $resultadoFornecedores = $conn->query($sqlFornecedores);
-                            while ($rowFornecedores = $resultadoFornecedores->fetch_assoc()) {
-                                $sqlPessoas = "SELECT nome_fantasia FROM pessoas WHERE cnpj = '" . $rowFornecedores['cnpj'] . "'";
-                                $resultadoPessoas = $conn->query($sqlPessoas);
-                                $nomeFornecedor = $resultadoPessoas->fetch_assoc();
+                            $sqlForn = "SELECT * FROM fornecedores WHERE status = 1";
+                            $resultadoForn = $conn->query($sqlForn);
+                            while ($rowForn = $resultadoForn->fetch_assoc()) {
+                                $sqlPess = "SELECT nome_fantasia FROM pessoas where cnpj = '" . $rowForn['cnpj'] . "'";
+                                $resultadoPess = $conn->query($sqlPess);
+                                $nomeForn =  $resultadoPess->fetch_assoc();
                                 echo "
-                                <option value='" . $rowFornecedores['id'] . "' " . (($fornecedor == $rowFornecedores['id']) ? 'selected' : '') . ">" . $nomeFornecedor['nome_fantasia'] . "</option>
+                                <option value='" . $rowForn['id'] . "'" . (($rowForn['id'] == $produto['fornecedor']) ? "selected" : "") . ">" . $nomeForn['nome_fantasia'] . "</option>
                                 ";
                             }
                             ?>
@@ -80,13 +95,13 @@ while ($rowProduto = $resultadoProduto->fetch_assoc()) {
                     <label for="marca">Marca</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-tags"></i></span>
-                        <select name="marca" id="marca">
+                        <select name="marca" id="marca" required>
                             <?php
                             $sqlMarca = "SELECT * FROM marcas WHERE status = 1";
                             $resultadoMarca = $conn->query($sqlMarca);
                             while ($rowMarca = $resultadoMarca->fetch_assoc()) {
                                 echo "
-                                <option value='" . $rowMarca['id'] . "' " . (($marca == $rowMarca['id']) ? 'selected' : '') . ">" . $rowMarca['nome'] . "</option>
+                                <option value='" . $rowMarca['id'] . "'" . (($rowMarca['id'] == $produto['marca']) ? "selected" : "") . ">" . $rowMarca['nome'] . "</option>
                                 ";
                             }
                             ?>
@@ -97,13 +112,13 @@ while ($rowProduto = $resultadoProduto->fetch_assoc()) {
                     <label for="grupo">Grupo</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-boxes-stacked"></i></span>
-                        <select name="grupo" id="grupo">
+                        <select name="grupo" id="grupo" required>
                             <?php
-                            $sqlGrupo = "SELECT * FROM grupos WHERE status = 1";
+                            $sqlGrupo = "SELECT * from grupos WHERE status = 1";
                             $resultadoGrupo = $conn->query($sqlGrupo);
                             while ($rowGrupo = $resultadoGrupo->fetch_assoc()) {
                                 echo "
-                                <option value='" . $rowGrupo['id'] . "' " . (($grupo == $rowGrupo['id']) ? 'selected' : '') . ">" . $rowGrupo['nome'] . "</option>
+                                <option value='" . $rowGrupo['id'] . "'" . (($rowGrupo['id'] == $produto['grupo']) ? "selected" : "") . ">" . $rowGrupo['nome'] . "</option>
                                 ";
                             }
                             ?>
@@ -114,24 +129,29 @@ while ($rowProduto = $resultadoProduto->fetch_assoc()) {
                     <label for="subgrupo">Sub Grupo</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-dolly"></i></span>
-                        <select name="subgrupo" id="subgrupo">
+                        <select name="subgrupo" id="subgrupo" required>
                             <?php
                             $sqlSubgrupo = "SELECT * FROM subgrupo WHERE status = 1";
                             $resultadoSubgrupo = $conn->query($sqlSubgrupo);
                             while ($rowSubgrupo = $resultadoSubgrupo->fetch_assoc()) {
                                 echo "
-                                <option value='" . $rowSubgrupo['id'] . "' " . (($subgrupo == $rowSubgrupo['id']) ? 'selected' : '') . ">" . $rowSubgrupo['nome'] . "</option>
+                                <option value='" . $rowSubgrupo['id'] . "'" . (($rowSubgrupo['id'] == $produto['subgrupo']) ? "selected" : "") . ">" . $rowSubgrupo['nome'] . "</option>
                                 ";
                             }
                             ?>
                         </select>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="form-container" id="large-form">
+            <h2 class="form-title">Preço e Estoque</h2>
+            <div class="card-form">
                 <div class="form-group">
                     <label for="preco_custo">Preço de Custo</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-money-bill"></i></span>
-                        <input type="number" step="0.01" class="form-control" value="<?php echo $preco_custo ?>"
+                        <input type="number" step="0.01" class="form-control" value="<?= $produto['preco_custo'] ?>"
                             name="preco_custo" id="preco_custo" placeholder="Digite o Preço de Custo" required>
                     </div>
                 </div>
@@ -139,7 +159,7 @@ while ($rowProduto = $resultadoProduto->fetch_assoc()) {
                     <label for="preco_venda">Preço de Venda</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-hand-holding-dollar"></i></span>
-                        <input type="number" step="0.01" class="form-control" value="<?php echo $preco_venda ?>"
+                        <input type="number" step="0.01" class="form-control" value="<?= $produto['preco_venda'] ?>"
                             name="preco_venda" id="preco_venda" placeholder="Digite o Preço de Venda" required>
                     </div>
                 </div>
@@ -147,32 +167,45 @@ while ($rowProduto = $resultadoProduto->fetch_assoc()) {
                     <label for="quantidade">Quantidade</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-list-ol"></i></span>
-                        <input type="number" class="form-control" value="<?php echo $quantidade ?>" name="quantidade"
-                            id="quantidade" placeholder="Digite a Quantidade" required>
+                        <input type="number" class="form-control" value="<?= $produto['quantidade'] ?>"
+                            name="quantidade" id="quantidade" placeholder="Digite a Quantidade" required>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="validade">Validade</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-calendar-check"></i></span>
-                        <input type="date" class="form-control" value="<?php echo $validade ?>" name="validade"
-                            id="validade" placeholder="Digite a Validade">
+                        <input type="date" class="form-control" value="<?= $produto['validade'] ?>" name="validade"
+                            id="validade" placeholder="Digite a Validade" required>
                     </div>
                 </div>
-                <div class="form-group">
-                    <label for="imagem">Link da imagem</label>
-                    <div class="input-group">
-                        <span class="input-group-text"><i class="fa-solid fa-image"></i></span>
-                        <input type="text" class="form-control" value="<?php echo $imagem ?>" name="imagem"
-                            id="imagem" placeholder="Cole o link da imagem">
-                    </div>
-                </div>
-                <input type="hidden" value="<?php echo $id ?>" name="id">
-                <button type="submit" class="btn btn-primary btn-block mt-3">Editar</button>
-            </form>
+            </div>
         </div>
+        <div class="btn-submit-container">
+            <input type="hidden" name="id" value="<?= $produto['id'] ?>">
+            <button type="submit" class="btn-block-large">Editar</button>
+        </div>
+        </form>
     </div>
-
+    </div>
+    <script>
+        document.getElementById('imagem').addEventListener('change', function(e) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                document.getElementById('preview').src = e.target.result;
+                document.getElementById('preview').style.display = 'block';
+            };
+            reader.readAsDataURL(this.files[0]);
+        });
+    </script>
+    <script>
+        <?php
+        if (isset($_SESSION['resposta'])) {
+            echo "alert('" . $_SESSION['resposta'] . "')";
+            unset($_SESSION['resposta']);
+        }
+        ?>
+    </script>
 </body>
 
 </html>
--- database/produtos/editar_produto.php
+++ database/produtos/editar_produto.php
@@ -13,22 +13,52 @@ $preco_venda = floatval($_POST['preco_venda']);
 $quantidade = intval($_POST['quantidade']);
 $validade = $_POST['validade'];
 $subgrupo = $_POST['subgrupo'];
-$imagem = $_POST["imagem"];
+$imagem = $_FILES['imagem']['tmp_name'];
+$file_name = $_FILES['imagem']['name'];
+$file_info = pathinfo($file_name);
+$extencao = strtolower($file_info['extension']);
+$file_type = $_FILES['imagem']['type'];
+$imagem_info = getimagesize($imagem);
+$imagem_type = $imagem_info[2];
+
+
+if ($imagem_type === IMAGETYPE_JPEG || $imagem_type === IMAGETYPE_PNG) {
+    if ((($extencao === 'jpg' || $extencao === 'jpeg') && ($file_type === 'image/jpeg'))) {
+        if ($imagem_type === IMAGETYPE_PNG) {
+            $imagem_original = imagecreatefrompng($imagem);
+        } else {
+            $imagem_original = imagecreatefromjpeg($imagem);
+        }
+        ob_start();
+        imagejpeg($imagem_original, null, 20);
+        $imagem_data = ob_get_clean();
+        imagedestroy($imagem_original);
+    } else {
+        $_SESSION['resposta'] = "Formato de imagem inválido!";
+        header("Location: ../../admin/produtos/produtos_adm.php");
+        exit;
+    }
+} else {
+    $_SESSION['resposta'] = "Formato de imagem inválido 2!";
+    header("Location: ../../admin/produtos/produtos_adm.php");
+    exit;
+}
 
 if (!DateTime::createFromFormat('Y-m-d', $validade)) {
     $_SESSION['resposta'] = "Data de validade inválida!";
-    header("Location: ../../admin/produtos/cadastro_produto.php");
+    header("Location: ../../admin/produtos/produtos_adm.php");
     exit;
 }
 
 $sql = "UPDATE produtos SET nome = ?, codigo_barra = ?, fornecedor = ?, preco_custo = ?, preco_venda = ?, quantidade = ?, subgrupo = ?, grupo = ?, marca = ?, validade = ?, imagem = ? WHERE id = ?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sssssssssssi", $nome, $codigo, $fornecedor, $preco_custo, $preco_venda, $quantidade, $subgrupo, $grupo, $marca, $validade, $imagem, $id);
+$stmt->bind_param("ssssssssssbi", $nome, $codigo, $fornecedor, $preco_custo, $preco_venda, $quantidade, $subgrupo, $grupo, $marca, $validade, $imagem_data, $id);
+$stmt->send_long_data(10, $imagem_data);
 
 if ($stmt->execute()) {
-    $_SESSION['resposta'] = "Produto editado com sucessso!";
+    $_SESSION['resposta'] = "Produto atualizado com sucesso!";
 } else {
-    $_SESSION['resposta'] = "Erro ao editar produto: " . $stmt->error;
+    $_SESSION['resposta'] = "Erro ao atualizar produto: " . $stmt->error;
 }
 
 header("Location: ../../admin/produtos/produtos_adm.php");
