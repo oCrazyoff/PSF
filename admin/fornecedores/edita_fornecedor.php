1166af1e9fa82a98c26c0d7a42f613c7085d34b1
oCrazyoff <walyssonribeiro3@gmail.com>
2025-01-29 19:43:17 -0300

    fornecedores funcionando

--- admin/fornecedores/edita_fornecedor.php
+++ admin/fornecedores/edita_fornecedor.php
@@ -4,27 +4,43 @@ include("../../auth/valida.php");
 include("../../database/utils/conexao.php");
 
 $cnpj = $_POST['cnpj'];
-$sqlFornecedores = "SELECT cnpj, razao_social, nome_fantasia, endereco, contato FROM pessoas WHERE cnpj = '$cnpj'";
-$resultadoFornecedores = $conn->query($sqlFornecedores);
+$sql = "SELECT * FROM pessoas WHERE cnpj = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $cnpj);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
 
-while ($rowFornecedores = $resultadoFornecedores->fetch_assoc()) {
-    $cnpj = $rowFornecedores["cnpj"];
-    $razao_social = $rowFornecedores["razao_social"];
-    $nome_fantasia = $rowFornecedores["nome_fantasia"];
-    $endereco = $rowFornecedores["endereco"];
-    $contato = $rowFornecedores["contato"];
+if ($row['endereco'] != null || $row['endereco'] != "") {
+    $partes = explode(", ", $row['endereco']);
+    $cep = $partes[0];
+    $logradouro = $partes[1];
+    $numero = $partes[2];
+    $bairro = $partes[3];
+    $complemento = count($partes) == 7 ? $partes[4] : "";
+    $cidade = count($partes) == 7 ? $partes[5] : $partes[4];
+    $estado = count($partes) == 7 ? $partes[6] : $partes[5];
+} else {
+    $cep = "";
+    $logradouro = "";
+    $numero = "";
+    $bairro = "";
+    $complemento = "";
+    $cidade = "";
+    $estado = "";
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar <?php echo $razao_social ?></title>
+    <title>Editar <?= htmlentities($row['nome_fantasia']) ?></title>
     <?php include("../../includes/link_head.php") ?>
-    <link rel="stylesheet" href="../../assets/css/form.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../../assets/css/form.css">
 </head>
 
 <body>
@@ -32,54 +48,109 @@ while ($rowFornecedores = $resultadoFornecedores->fetch_assoc()) {
     <?php include("../../includes/menu.php") ?>
     <div class="content">
         <div class="form-container" id="large-form">
-            <h2 class="form-title">Editar <?php echo $razao_social ?></h2>
+            <h2 class="form-title">Editar <?= htmlentities($row['nome_fantasia']) ?></h2>
             <form action="../../database/fornecedores/editar_fornecedor.php" method="post">
                 <div class="form-group">
                     <label for="cnpj">CNPJ</label>
                     <div class="input-group">
+                        <input type="hidden" name="cnpjAntigo" value="<?= htmlentities($row['cnpj']) ?>">
                         <span class="input-group-text"><i class="fa-regular fa-id-card"></i></span>
-                        <input type="text" class="form-control" value="<?php echo $cnpj ?>" name="cnpj" id="cnpj"
-                            placeholder="Digite o CNPJ" required>
+                        <input type="text" class="form-control" value="<?= htmlentities($row['cnpj']) ?>" name="cnpj"
+                            id="cnpj" placeholder="Digite sua CNPJ" required>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="razao_social">Razão Social</label>
                     <div class="input-group">
-                        <span class="input-group-text"><i class="fa-solid fa-signature"></i></span>
-                        <input type="text" class="form-control" value="<?php echo $razao_social ?>" name="razao_social"
-                            id="razao_social" placeholder="Digite a Razão Social">
+                        <span class="input-group-text"><i class="fa-solid fa-file-signature"></i></span>
+                        <input type="text" class="form-control" value="<?= htmlentities($row['razao_social']) ?>"
+                            name="razao_social" id="razao_social" placeholder="Digite sua Razão Social" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="nome_fantasia">Nome Fantásia</label>
+                    <label for="nome_fantasia">Nome Fantasia</label>
                     <div class="input-group">
-                        <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
-                        <input type="text" class="form-control" value="<?php echo $nome_fantasia ?>" name="nome_fantasia"
-                            id="nome_fantasia" placeholder="Digite o Nome Fantásia">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="endereco">Endereço</label>
-                    <div class="input-group">
-                        <span class="input-group-text"><i class="fa-solid fa-location-dot"></i></span>
-                        <input type="text" step="0.01" class="form-control" value="<?php echo $endereco ?>"
-                            name="endereco" id="endereco" placeholder="Digite o Endereço" required>
+                        <span class="input-group-text"><i class="fa-solid fa-pen-to-square"></i></span>
+                        <input type="text" class="form-control" value="<?= htmlentities($row['nome_fantasia']) ?>"
+                            name="nome_fantasia" id="nome_fantasia" placeholder="Digite seu Nome Fantásia" required>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="contato">Contato</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-phone"></i></span>
-                        <input type="text" class="form-control" value="<?php echo $contato ?>" name="contato"
-                            id="contato" placeholder="Digite o Contato">
+                        <input type="text" class="form-control" value="<?= htmlentities($row['contato']) ?>"
+                            name="contato" id="contato" placeholder="Digite seu Contato" required>
                     </div>
                 </div>
-                <input type="hidden" value="<?php echo $cnpj ?>" name="cnpjAtual">
-                <button type="submit" class="btn btn-primary btn-block mt-3">Editar</button>
-            </form>
         </div>
+        <div class="form-container" id="large-form">
+            <h2 class="form-title">Endereço</h2>
+            <div class="card-form">
+                <div class="form-group">
+                    <label for="cep">CEP</label>
+                    <div class="input-group">
+                        <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                        <input type="text" class="form-control" value="<?= htmlentities($cep) ?>" name="cep" id="cep"
+                            maxlength="9" placeholder="00000-000" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="logradouro">Logradouro</label>
+                    <div class="input-group">
+                        <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                        <input type="text" class="form-control" value="<?= htmlentities($logradouro) ?>"
+                            name="logradouro" id="logradouro" placeholder="Digite o Logradouro" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="numero">Número</label>
+                    <div class="input-group">
+                        <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                        <input type="number" class="form-control" value="<?= htmlentities($numero) ?>" name="numero"
+                            id="numero" placeholder="Digite o número" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="bairro">Bairro</label>
+                    <div class="input-group">
+                        <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                        <input type="text" class="form-control" value="<?= htmlentities($bairro) ?>" name="bairro"
+                            id="bairro" placeholder="Digite o bairro" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="complemento">Complemento(opcional)</label>
+                    <div class="input-group">
+                        <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                        <input type="text" class="form-control" value="<?= htmlentities($complemento) ?>"
+                            name="complemento" id="complemento" placeholder="Digite o completo">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="estado">Estado</label>
+                    <div class="input-group">
+                        <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                        <input type="text" class="form-control" value="<?= htmlentities($estado) ?>" name="estado"
+                            id="estado" placeholder="Digite o estado" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="cidade">Cidade</label>
+                    <div class="input-group">
+                        <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                        <input type="text" class="form-control" value="<?= htmlentities($cidade) ?>" name="cidade"
+                            id="cidade" placeholder="Digite o cidade" required>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="btn-submit-container">
+            <button type="submit" class="btn-block-large">Editar</button>
+        </div>
+        </form>
     </div>
-
+    </div>
 </body>
 
 </html>
--- database/fornecedores/cadastrar_fornecedor.php
+++ database/fornecedores/cadastrar_fornecedor.php
@@ -1,35 +1,48 @@
 <?php
 include("../utils/conexao.php");
+include("../../auth/valida.php");
 
-$cnpj = $_POST["cnpj"];
-$razao_social = $_POST["razao_social"];
-$nome_fantasia = $_POST["nome_fantasia"];
-$endereco = $_POST["endereco"];
-$contato = $_POST["contato"];
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+    $razao_social = filter_input(INPUT_POST, 'razao_social', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $nome_fantasia = filter_input(INPUT_POST, 'nome_fantasia', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $cnpj = filter_input(INPUT_POST, 'cnpj', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $cep = filter_input(INPUT_POST, 'cep', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $logradouro = filter_input(INPUT_POST, 'logradouro', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $numero = filter_input(INPUT_POST, 'numero', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $bairro = filter_input(INPUT_POST, 'bairro', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $complemento = filter_input(INPUT_POST, 'complemento', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $estado = filter_input(INPUT_POST, 'estado', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $cidade = filter_input(INPUT_POST, 'cidade', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $contato = filter_input(INPUT_POST, 'contato', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $tipo_pessoa = 2;
+    $cargo = 5;
+    $endereco = "$cep, $logradouro, $numero, $bairro, " . ($complemento == null ? "" : $complemento . ", ") . "$estado, $cidade";
 
-$sql = "INSERT INTO pessoas (cnpj, razao_social, nome_fantasia, endereco, contato, tipo_pessoa, cargo) VALUES (?, ?, ?, ?, ?, 2, 5)";
-$stmt = $conn->prepare($sql);
+    if ((isset($_POST["cnpj"])) and (!empty($_POST))) {
 
-$sql_fornecedor = "INSERT INTO fornecedores (cnpj) VALUES (?)";
-$stmt_fornecedor = $conn->prepare($sql_fornecedor);
+        $sqlPessoas = "INSERT INTO pessoas (cnpj, tipo_pessoa, cargo, contato, endereco, razao_social, nome_fantasia) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $stmtPessoas = $conn->prepare($sqlPessoas);
+        $stmtPessoas->bind_param("siissss", $cnpj, $tipo_pessoa, $cargo, $contato, $endereco, $razao_social, $nome_fantasia);
 
-if (!$stmt or !$stmt_fornecedor) {
-    die("Erro ao preparar a consulta: " . $conn->error);
-}
+        if ($stmtPessoas->execute()) {
+            $sql = "INSERT INTO fornecedores (cnpj) VALUES (?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $cnpj);
 
-$stmt->bind_param("sssss", $cnpj, $razao_social, $nome_fantasia, $endereco, $contato);
-
-$stmt_fornecedor->bind_param("s", $cnpj);
-
-if ($stmt->execute() and $stmt_fornecedor->execute()) {
-    $resposta = "Fornecedor cadastrado com sucesso!";
-    header("Location: ../../admin/fornecedores/fornecedores.php?resposta=$resposta");
-    die();
+            if ($stmt->execute()) {
+                $_SESSION['resposta'] = "Fornecedor cadastrado com sucessso!";
+            } else {
+                $_SESSION['resposta'] = "Erro ao cadastrar fornecedor: " . $stmtPessoas->error;
+            }
+        } else {
+            $_SESSION['resposta'] = "Erro ao cadastrar fornecedor: " . $stmt->error;
+        }
+    } else {
+        $_SESSION['resposta'] = "Variável POST ínvalida!";
+    }
 } else {
-    $resposta = "Fornecedor deu erro!";
-    header("Location: ../../admin/fornecedores/fornecedores.php?resposta=$resposta");
-    die();
+    $_SESSION['resposta'] = "Método de solicitação ínvalido!";
 }
 
-$stmt->close();
-$conn->close();
+header("Location: ../../admin/fornecedores/fornecedores.php");
+exit();
